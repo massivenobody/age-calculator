cc4c6b27837d1a1b01472f7cc1f806d0d2985493
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-03-29 15:02:06 -0500

    feat: added state to calculator
    - Calculator knows the birth date set.
    - Picking a different date updates state in calculator.

--- src/components/AgeCalculator.tsx
+++ src/components/AgeCalculator.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+
 function AgeCalculator() {
+  const [birthDate, setBirthDate] = useState<string>('');
+
+  function handleBirthDateChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newDate = e.target.value;
+    setBirthDate(newDate);
+  }
+
   return (
     <div className="age-calculator">
       <h1>Age Calculator</h1>
       <div className="age-calculator-picker">
         <label htmlFor="date-of-birth">Enter your birth date:</label>
-        <input type="date" id="date-of-birth" />
-        <button>Calculate</button>
+        <input
+            type="date"
+            id="date-of-birth"
+            aria-label="Date of birth"
+            value={birthDate}
+            onChange={handleBirthDateChange}
+        />
       </div>
-        <p className="age-result">You are <span id="age-calculated"></span> years old</p>
+        <p className="age-result">You are <span id="age-calculated"></span> old</p>
     </div>
   )
 }
