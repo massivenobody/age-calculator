bc58924ef93898eb3966d387daa9a7aceec54b86
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-03-29 16:03:41 -0500

    feat: add age calculation features
    - Calculate time since birth date using luxon.
    - Implement function to get gramatically correct age sentence.

--- src/components/AgeCalculator.tsx
+++ src/components/AgeCalculator.tsx
@@ -1,11 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { DateTime, DurationObjectUnits } from 'luxon';
 
 function AgeCalculator() {
-  const [birthDate, setBirthDate] = useState<string>('');
+  const [birthDateString, setBirthDateString] = useState<string>('');
+  const birthDate = DateTime.fromFormat(birthDateString, 'yyyy-MM-dd');
+  // Calculate diff and negate to get positive values.
+  const diff = birthDate.diffNow(['years', 'months', 'days']).negate().toObject();
+
+  // Show result if date is valid and all diff values are positive
+  let showResult = birthDate.isValid && Object.keys(diff).length > 0;
+  if (showResult) {
+    // Round down days to whole number
+    diff.days = Math.floor(diff.days!);
+    showResult = (diff.years! > 0 || diff.months! > 0 || diff.days! > 0);
+  }
 
   function handleBirthDateChange(e: React.ChangeEvent<HTMLInputElement>) {
     const newDate = e.target.value;
-    setBirthDate(newDate);
+    setBirthDateString(newDate);
+  }
+
+  function getAgeString(diff: DurationObjectUnits) {
+    let finalString = '';
+    if (diff.years! > 0) {
+        finalString += `${diff.years} year`;
+        if (diff.years! > 1) finalString += 's';
+        if (diff.months! > 0 && diff.days! > 0) finalString += ', ';
+        if (diff.months! > 0 && diff.days! <= 0) finalString += ' and ';
+        if (diff.months! <= 0 && diff.days! > 0) finalString += ' and ';
+    }
+    if (diff.months! > 0) {
+        finalString += `${diff.months} month`;
+        if (diff.months! > 1) finalString += 's';
+        if (diff.years! > 0 && diff.days! > 0) finalString += ',';
+        if (diff.days! > 0) finalString += ' and ';
+    }
+    if (diff.days! > 0) {
+        finalString += `${diff.days} day`;
+        if (diff.days! > 1) finalString += 's';
+    }
+    return finalString;
   }
 
   return (
@@ -17,11 +51,17 @@ function AgeCalculator() {
             type="date"
             id="date-of-birth"
             aria-label="Date of birth"
-            value={birthDate}
+            value={birthDateString}
             onChange={handleBirthDateChange}
         />
       </div>
-        <p className="age-result">You are <span id="age-calculated"></span> old</p>
+      {
+        showResult
+        ? (
+        <p className="age-result">You are <span id="age-calculated">{getAgeString(diff)}</span> old</p>
+        )
+        : null
+      }
     </div>
   )
 }
