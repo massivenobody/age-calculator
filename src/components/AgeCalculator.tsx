042bc322f017afe3ed5849fae14146089b46b8fb
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-03-29 13:54:30 -0500

    feat: add placeholder jsx
    - Add placeholder JSX to be returned when calling calculator component

--- src/components/AgeCalculator.tsx
+++ src/components/AgeCalculator.tsx
@@ -1,7 +1,13 @@
 function AgeCalculator() {
   return (
-    <div>
+    <div className="age-calculator">
       <h1>Age Calculator</h1>
+      <div className="age-calculator-picker">
+        <label htmlFor="date-of-birth">Enter your birth date:</label>
+        <input type="date" id="date-of-birth" />
+        <button>Calculate</button>
+      </div>
+        <p className="age-result">You are <span id="age-calculated"></span> years old</p>
     </div>
   )
 }
